c96db8f690f3262b34727ea0a9792da8fc8ee657
shivam910 <shivams91099@gmail.com>
2021-08-05 12:37:39 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -89,12 +89,12 @@ export default function Navbar() {
       </div>
 
       <div className="bg-white shadow">
-        <div className="flex max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">आकर्षण-</h1>
+        <div className="flex max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 ">
+          <h1 className="text-3xl font-bold text-gray-900 hover:text-purple-700">आकर्षण-</h1>
           <div className="flex-shrink-0">
             <img className="h-9 w-9 mx-1" src={India} alt="image" />
           </div>
-          <h1 className="text-3xl font-bold text-gray-900 mx-3">Home</h1>
+          <h1 className="text-3xl font-bold text-gray-900 mx-3 ">Home</h1>
         </div>
       </div>
     </div>
